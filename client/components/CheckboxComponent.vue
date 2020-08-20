774c2d0b5f7460e1e4296413d0957c63b9e56a41
Gopi Krishna M <gopt.muktevi@gmail.com>
2020-08-20 16:25:40 +0530

    Adding some components and props

---
+++ client/components/CheckboxComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="text-center">{{ personName }} as {{ title }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['personName'],
+    data() {
+        return {
+            title: 'check me'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- client/components/HelloComponent.vue
+++ client/components/HelloComponent.vue
@@ -3,13 +3,18 @@
         <div class="row my-5 justify-content-center">
             <div class="col-sm-6">
                 <h1 class="text-center">Welcome to the world of Vue</h1>
+                <checkbox-component personName="James"></checkbox-component>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import CheckboxComponent from './CheckboxComponent';
 export default {
+    components: {
+        'checkbox-component': CheckboxComponent
+    }
 }
 </script>
 
